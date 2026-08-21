84e1c9079edd4bd473495cb5685b44f89027c399
sisicity4 <sisicity4@users.noreply.github.com>
2026-08-21 10:10:06 +0900

    画像の追加、djangoの方引数エラーの極小修正

--- shunkan_project/settings.py
+++ shunkan_project/settings.py
@@ -47,7 +47,7 @@ WSGI_APPLICATION = "shunkan_project.wsgi.application"
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
-AUTH_PASSWORD_VALIDATORS: list[dict] = []
+AUTH_PASSWORD_VALIDATORS: list[dict[str, str]] = []
 LANGUAGE_CODE = "ja"
 TIME_ZONE = "Asia/Tokyo"
 USE_I18N = True
