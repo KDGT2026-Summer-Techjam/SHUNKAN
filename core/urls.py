99b7db9bd2c772356eedbba90240d0cac22041c1
ayato <ayato.211210@gmail.com>
2026-08-25 15:16:47 +0900

    T-16a完成

--- core/urls.py
+++ core/urls.py
@@ -6,14 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("accounts/signup/", views.signup, name="signup"),
-    path(
-        "accounts/login/",
-        auth_views.LoginView.as_view(
-            template_name="core/login.html",
-            redirect_authenticated_user=True,
-        ),
-        name="login",
-    ),
+    path("accounts/login/",auth_views.LoginView.as_view(template_name="core/login.html",redirect_authenticated_user=True,),name="login",),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("rooms/", views.rooms, name="rooms"),
     path("rooms/<int:room_id>/", views.room_detail, name="room_detail"),
